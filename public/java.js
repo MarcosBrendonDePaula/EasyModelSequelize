21f115dfa11899ce17df5957f70a4a1607d79181
Marcos Brendon De Paula <marcos.brendon@aluno.ufms.br>
2021-02-19 00:04:40 -0300

    icon

--- public/java.js
+++ public/java.js
@@ -239,3 +239,26 @@ function LoadModels() {
 }
 
 document.querySelector('.ID').value = Math.floor(Math.random() * 8000)
+
+
+function download(data, filename="database.json", type='text/plain') {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
+
+function DownloadBackup() {
+    download(document.querySelector('.FormModels').value)
+}
